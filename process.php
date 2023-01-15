5d09e1354472fb334acbbc9885b91659bbdc1431
DesignWith6ix <88059404+6ixtus@users.noreply.github.com>
2023-01-15 19:28:15 +0100

    hh

--- process.php
+++ process.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
 
     <?php 
 	
@@ -35,5 +27,3 @@ if (isset($_POST['import'])) {
 }
 
 ?>
-</body>
-</html>
